f08e169e423adc44f800955f3149596f39bd2b35
Tom Jakubowski <tom@crystae.net>
2015-01-26 11:54:51 -0800

    Complete lab 1 up to "The Stack"

--- kern/init.c
+++ kern/init.c
@@ -35,6 +35,10 @@ i386_init(void)
 
 	cprintf("6828 decimal is %o octal!\n", 6828);
 
+	{
+		// testing code goes here
+	}
+
 	// Test the stack backtrace function (lab 1 only)
 	test_backtrace(5);
 
